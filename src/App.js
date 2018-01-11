84e72f8724faca0ce63b3a26ed361f4f62f437bb
CrazyBOY <thammawat_bee@hotmail.com>
2018-01-11 19:37:32 +0700

    add setting module

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import './App.css';
 import busStop from './icon.png'
 
 class App extends Component {
+
+  addGps=() => {
+    console.log("hello")
+  }
   render() {
     return (
       <div>
        <h1>project</h1>
+       <button onClick={()=>{this.addGps()}}>snap to road</button>
       </div>
     );
   }
